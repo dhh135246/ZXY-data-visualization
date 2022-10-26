1220a3f23341c0928a6d9bffafe59995d895e910
unknown <dhh135246@163.com>
2022-10-27 03:31:06 +0800

    ctmd!

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.573be5b13df3d252e40bd9ee080caccd.js"
+  "precache-manifest.73b947e7b8b660546cefe14d1b96d510.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "ZXY-data-visualization"});
